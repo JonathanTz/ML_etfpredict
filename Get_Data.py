39f47eac970a3d3dcccb85cfa77e6bfe043c9b97
JonathanTz <dog820507@yahoo.com.tw>
2018-04-25 19:34:03 +0800

    function of get tick data

---
+++ Get_Data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+data=pd.read_csv("Data/taetfp.csv",encoding='big5')
+data.columns=['tick','date','name','open','high','low','close','volume']
+data.name=pd.DataFrame([(lambda x:data.name[x].strip())(x) for x in range(len(data.name))])
+def GetTick(dt):
+    dt1=dt.groupby(['name'])['tick'].mean()
+    print(dt1)
+
+GetTick(data)
+def etf_p(*args):
+    ticks=input('enter the tick you want')
+    price=data[data.tick==int(ticks)]
+    return price
+print(etf_p())
