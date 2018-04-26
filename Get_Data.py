1497a12cfbfcbb29bd8415adaec0aad5edbfa759
JonathanTz <dog820507@yahoo.com.tw>
2018-04-26 10:35:51 +0800

    function of get data

--- Get_Data.py
+++ Get_Data.py
@@ -1,15 +1,23 @@
 import pandas as pd
-data=pd.read_csv("Data/taetfp.csv",encoding='big5')
-data.columns=['tick','date','name','open','high','low','close','volume']
-data.name=pd.DataFrame([(lambda x:data.name[x].strip())(x) for x in range(len(data.name))])
-def GetTick(dt):
-    dt1=dt.groupby(['name'])['tick'].mean()
-    print(dt1)
+import os
 
-GetTick(data)
-def etf_p(*args):
-    ticks=input('enter the tick you want')
-    price=data[data.tick==int(ticks)]
-    return price
-print(etf_p())
-a=1
+fileNo={1:'taetfp',2:'tasharep',3:'tetfp',4:'tsharep'}
+def get_data(idx):
+      ##check whether pickle exist
+      if not os.path.isfile(fileNo[idx]+'.pickle'):
+            ###there is a error if encoding=big5
+            data=pd.read_csv("Data/"+fileNo[idx]+".csv",encoding='cp950')
+            data.columns=['tick','date','name','open','high','low','close','volume']
+            data.name=pd.DataFrame([(lambda x:data.name[x].strip())(x) for x in range(len(data.name))])
+            data.to_pickle(fileNo[idx]+'.pickle')
+      data=pd.read_pickle(fileNo[idx]+'.pickle')
+      dt1=data.groupby(['name'])['tick'].mean()
+      
+      ###Show all ticks, and you should input the ticks u want,
+      print(dt1)
+      ticks=input('enter the tick you want')
+      ###
+
+      price=data[data.tick==int(ticks)]
+      return price
+print(get_data(2))
