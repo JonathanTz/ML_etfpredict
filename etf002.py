3c0fd459876f07e0c442b2c8f1197364d67c724f
JonathanTz <dog820507@yahoo.com.tw>
2018-05-07 16:00:03 +0800

    modify etf002.py (data by dayof week)

--- etf002.py
+++ etf002.py
@@ -7,11 +7,11 @@ Created on Mon Apr 30 19:49:38 2018
 
 import pandas as pd
 import numpy as np
-from sklearn.cross_validation import train_test_split
+#from sklearn.cross_validation import train_test_split
 #from sklearn.model_selection import train_test_split #新的import方法，上面的好像要被除掉?
-from sklearn.learning_curve import learning_curve #使用learning_curve模組來可視化機器學習過程
-from sklearn.svm import SVR #導入支持向量機模型
-from sklearn import preprocessing #要進行標準化則要先導入這個套件
+#from sklearn.learning_curve import learning_curve #使用learning_curve模組來可視化機器學習過程
+#from sklearn.svm import SVR #導入支持向量機模型
+#from sklearn import preprocessing #要進行標準化則要先導入這個套件
 import matplotlib.pyplot as plt #導入畫圖模組
 from datetime import datetime
 
@@ -46,7 +46,25 @@ df_target = pd.DataFrame({
                     'Friday':df_close_FRI
                     })
 
+print(df_target)
+##creat dataframe with all date in sample period
+raw_date=pd.DataFrame(index=pd.date_range('1/7/2013','5/3/2018'))
+merge_date=pd.concat([df,df_target],axis=1)
+##add a column record dayofweek
+merge_date['day_week']=merge_date.index.dayofweek
+##refresh date from dayofweek=0 to dayofweek=4
+start=merge_date[merge_date.day_week==0].index[0].date()
+end=merge_date[merge_date.day_week==4].index[len(merge_date[merge_date.day_week==4])-1].date()
+merge_date=merge_date[start:end]
 
+all_data=pd.concat([merge_date['Monday'].reset_index(drop=True),
+                    merge_date['Tuesday'].shift(-1).reset_index(drop=True),
+                    merge_date['Wednesday'].shift(-2).reset_index(drop=True),
+                    merge_date['Thursday'].shift(-3).reset_index(drop=True),
+                    merge_date['Friday'].shift(-4).reset_index(drop=True),
+                    ],axis=1)
+all_data.index=merge_date['Monday'].index
+all_data=all_data.dropna(axis=0,how='all')
 '''
 #模型建構
 
